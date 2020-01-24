0571a7031d52f6dbfb0970986497c314a95ebb9e
Maksim <max3691@yandex.ru>
2020-01-23 21:05:09 -0500

    Revert "lab 2"
    This reverts commit e29a3b2490ba7ab3a07dcce521084183670eb9c2.

--- server.py
+++ server.py
@@ -11,7 +11,6 @@ connected = 0
 
 clients = {}
 
-#  message loop 
 def connectionLoop(sock):
    while True:
       data, addr = sock.recvfrom(1024)
@@ -20,59 +19,33 @@ def connectionLoop(sock):
          if 'heartbeat' in data:
             clients[addr]['lastBeat'] = datetime.now()
       else:
-         # new client
          if 'connect' in data:
             clients[addr] = {}
             clients[addr]['lastBeat'] = datetime.now()
-            clients[addr]['color'] = {"RED": random.random(), "GREEN": random.random(), "BLUE": random.random()}
-            
-             message = {"cmd": 0,"players":[{"id":str(addr), "color": clients[addr]['color']}]}#,"position": clients[addr]['position']}]}
+            clients[addr]['color'] = 0
+            message = {"cmd": 0,"player":{"id":str(addr)}}
             m = json.dumps(message)
             for c in clients:
-               if c != addr :
-                  print(m)
-                  sock.sendto(bytes(m,'utf8'), (c[0],c[1]))
-            
-            print('Player is connected')
-            Spawn = {"cmd": 2, "players": []}
-            for c in clients:
-               player = {}
-               player['id'] = str(c)
-               player['color'] = clients[c]['color']
-             Spawn['players'].append(player)
-            playerInfo=json.dumps(Spawn)
-            print(playerInfo)
-            sock.sendto(bytes(playerInfo,'utf8'), (addr[0],addr[1]))
+               sock.sendto(bytes(m,'utf8'), (c[0],c[1]))
 
-
-def cleanClients(sock):
+def cleanClients():
    while True:
-      deleteMessage = {"cmd": 2,"players":[]}
       for c in list(clients.keys()):
          if (datetime.now() - clients[c]['lastBeat']).total_seconds() > 5:
-            print('Player Left Clients: ', c)
-            player = {}
-            player['id'] = str(c)
-            player['color'] = clients[c]['color']
-            deleteMessage['players'].append(player)
+            print('Dropped Client: ', c)
             clients_lock.acquire()
             del clients[c]
             clients_lock.release()
-
-      dm = json.dumps(deleteMessage)
-      for f in clients:
-         sock.sendto(bytes(dm,'utf8'), (f[0],f[1]))
-
       time.sleep(1)
-
 
 def gameLoop(sock):
    while True:
       GameState = {"cmd": 1, "players": []}
       clients_lock.acquire()
+      print (clients)
       for c in clients:
          player = {}
-         clients[c]['color'] = {"RED": random.random(), "GREEN": random.random(), "BLUE": random.random()}
+         clients[c]['color'] = {"R": random.random(), "G": random.random(), "B": random.random()}
          player['id'] = str(c)
          player['color'] = clients[c]['color']
          GameState['players'].append(player)
@@ -89,7 +62,7 @@ def main():
    s.bind(('', port))
    start_new_thread(gameLoop, (s,))
    start_new_thread(connectionLoop, (s,))
-   start_new_thread(cleanClients,(s,))
+   start_new_thread(cleanClients,())
    while True:
       time.sleep(1)
 
